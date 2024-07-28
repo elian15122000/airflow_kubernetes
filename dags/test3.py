4a574ec6012173eeff5d46e9302e419ccfa7ebfd
elian15122000 <elian.yildirim@web.de>
2024-07-28 02:16:24 +0200

    Create test3.py

---
+++ dags/test3.py
@@ -0,0 +1,27 @@
+
+from airflow import DAG
+from airflow.operators.dummy import DummyOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2023, 1, 1),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG(
+    'check_time',
+    default_args=default_args,
+    description='A simple test DAG',
+    schedule_interval=timedelta(days=1),
+)
+
+t1 = DummyOperator(
+    task_id='dummy_task',
+    dag=dag,
+)
